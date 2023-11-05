46e2692430fc2e6d33378f7a52483ab6c19e54d7
Nexus <129723215+nex4s@users.noreply.github.com>
2023-11-05 01:01:56 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MainPage from './components/mainPage'
+import '../../todolist/src/css/Particles.css'
+export default function App() {
+  return (
+    <html lang='en' className='h-screen'>
+      
+<div class="space">
+<div class="particle"></div>
+<MainPage></MainPage>
+  <div class="particle"></div>
+</div>
+    
+    </html>
+  )
+}
